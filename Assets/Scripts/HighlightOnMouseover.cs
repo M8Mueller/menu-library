1a251633923cf4c8918b90612f0f3bbc1fa50a07
sc-mmueller <mmueller@stonecobra.com>
2016-01-26 22:06:26 -0800

    added files

---
+++ Assets/Scripts/HighlightOnMouseover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HighlightOnMouseover : MonoBehaviour {
+
+	private Color startcolor;
+
+	void OnMouseEnter(){
+		startcolor = GetComponent<Renderer>().material.color;
+		GetComponent<Renderer>().material.color = Color.cyan;
+	}
+
+	void OnMouseExit(){
+		GetComponent<Renderer>().material.color = startcolor;
+	}
+}
